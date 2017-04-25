7e89770f31d99f4f5837d1a38bbf97f0cfac63c7
VaneGarciaYara <vanegarciay@gmail.com>
2017-04-24 22:28:42 -0300

    Primer MVP del convertidor de Celsius a fahrenheit, funcional

---
+++ README.md
@@ -0,0 +1,3 @@
+## Convertidor de Celsius a fahrenheit
+
+_Por: Vanessa Garcia Yara_
--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,1 +1,5 @@
-alert("Hola Mundo");
+function convertirCelsius(){
+    var celsius = document.getElementById('celsius').value; 
+    var fahrenheit = Math.round((celsius*1.8)+32);
+    document.getElementById('fahrenheit').value = fahrenheit;  
+}
